94f93258dcfa5921829b0ca147b64eec71c570db
Thi Nguyen <anhthi.ieig@gmail.com>
2021-10-03 11:19:56 +0700

    Change default formatter

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,11 @@ import React from 'react';
 import styles from './home.scss';
 
 const Home = () => {
-  return <div className={styles.home}>This is home page</div>;
+  return (
+    <div className={styles.home}>
+      <p className={styles.title}>This is home page</p>
+    </div>
+  );
 };
 
 export default Home;
